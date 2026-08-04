efe705967748ec4a8e7341dfb2c7f603c0cb2fee
Guido <guido-visser@hotmail.com>
2026-08-04 22:33:32 +0200

    added perlin noise texture

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "raytracer.h"
+#include "perlin.h"
 #include "texture.h"
 #include "camera.h"
 #include "bvh.h"
@@ -130,11 +131,38 @@ void earth()
     cam.render(hittable_list(globe));
 }
 
+void perlin_spheres()
+{
+    hittable_list world;
+
+    auto perlin_texture = make_shared<noise_texture>(4);
+    auto checker = make_shared<checker_texture>(0.32, color(.2, .3, .1), color(.9, .9, .9));
+    world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, make_shared<lambertian>(checker)));
+    world.add(make_shared<sphere>(point3(0, 2, 0), 2, make_shared<lambertian>(perlin_texture)));
+
+    camera cam;
+
+    cam.aspect_ratio        = 16.0 / 9.0;
+    cam.image_width         = 600;
+    cam.samples_per_pixel   = 100;
+    cam.max_recursion_depth = 50;
+
+    cam.vfov     = 20;
+    cam.lookfrom = point3(13,2,3);
+    cam.lookat   = point3(0,0,0);
+    cam.vup      = vec3(0,1,0);
+
+    cam.defocus_angle = 0;
+
+    cam.render(world);
+}
+
 int main()
 {
-    switch (3) {
+    switch (4) {
         case 1: bouncing_spheres();  break;
         case 2: checkered_spheres(); break;
         case 3: earth(); break;
+        case 4: perlin_spheres(); break;
     }
 }
--- raytracer.h
+++ raytracer.h
@@ -34,7 +34,7 @@ inline double random_double(double min, double max)
 
 inline int random_int(int min, int max)
 {
-    int(random_double(min, max+1));
+    return int(random_double(min, max+1));
 }
 
 // Common headers
